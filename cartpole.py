77cafbfcc220e5eec9c62638c8626dc958192259
Wesley Burlani <wesleyburlani@Wesleys-MacBook-Air.local>
2018-04-15 17:08:36 -0300

    solved cartpole problem

--- cartpole.py
+++ cartpole.py
@@ -1,8 +1,4 @@
 import numpy as np
-try:
-  import cPickle as pickle
-except ImportError:
-  import pickle as pickle
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import math
@@ -12,120 +8,114 @@ from collections import Counter
 import tflearn
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Activation
 
-def CreateEnviroment():
-	enviroment = gym.make('CartPole-v0')
-	enviroment.reset()
-	return enviroment;
+class Cartpole:
+    def __init__(self):
+        self.learnRate = 1e-3
+        self.enviroment = gym.make('CartPole-v0')
+        self.enviroment.reset()
+        self.scoreRequirement = 100
+        self.initialGames = 50 
+    
+    def InitialPopulation(self):
+        trainingData = []
+        scores = []
+        acceptedScores = []
+        while(len(acceptedScores) < self.initialGames):
+            score = 0
+            gameMemory = []
+            previousObservation = [] 
 
-learnRate = 1e-3
-enviroment = CreateEnviroment()
-goalSteps = 500
-scoreRequirement = 50
-initialGames = 10000 
-randomEpisodes = 0
-rewardSum = 0
+            done = False
+            while not done: 
+                action = self.enviroment.action_space.sample()
+                observation, reward, done, info = self.enviroment.step(action)
 
-def RunRandomAttempts():
-	while randomEpisodes < 100:
-		observation = enviroment.reset() 
-		for step in range(goalSteps):
-			enviroment.render() # render current enviroment.
-			action = enviroment.action_space.sample() # get a random action.. will go to right side, or left, whatever..
-			observation, reward, done, info = enviroment.step(action) # do some random action ..
-			if done:
-				print("Episode finished after {} timesteps".format(step+1))
-				break
+                if len(previousObservation) > 0 :  #is not the first iteration
+                    gameMemory.append([previousObservation, action]) # I've applied this action at this scenario.
+                    
+                previousObservation = observation
+                score+=reward # how many iterations to done? 
+                if done : 
+                    break
 
-def InitialPopulation():
-	trainingData = []
-	scores = []
-	acceptedScores = []
-	for _ in range(initialGames):
-		score = 0
-		gameMemory = []
-		previousObservation = [] 
-	
-		for _ in range(goalSteps):
-			action = enviroment.action_space.sample()
-			observation, reward, done, info = enviroment.step(action)
+            if score >= self.scoreRequirement : 
+                acceptedScores.append(score)
+                for data in gameMemory:
+                    if data[1] == 1: # if car go to the right side...
+                        output = [0,1]
+                    elif data[1] == 0: # if car go to the left side..
+                        output = [1,0]		
+                    trainingData.append([data[0], output])	
+                
+            self.enviroment.reset()
+            scores.append(score)
 
-			if len(previousObservation) > 0 :  #is not the first iteration
-				gameMemory.append([previousObservation, action]) # I've applied this action at this scenario.
-				
-			previousObservation = observation
-			score+=reward # how many iterations to done? 
-			if done : 
-				break
+        trainingDataSave = np.array(trainingData)
+        np.save('saved.npy', trainingDataSave)
 
-		if score >= scoreRequirement : 
-			acceptedScores.append(score)
-			for data in gameMemory:
-				if data[1] == 1: # if car go to the right side...
-					output = [0,1]
-				elif data[1] == 0: # if car go to the left side..
-					output = [1,0]		
-				trainingData.append([data[0], output])	
-			
-		enviroment.reset()
-		scores.append(score)
+        print('Average Accpeted score: ', mean(acceptedScores))
+        print('Median accpeted score: ', median(acceptedScores))
+        print(Counter(acceptedScores))
+        return trainingData
 
-	trainingDataSave = np.array(trainingData)
-	np.save('saved.npy', trainingDataSave)
+    def BuildNeuralNetworkModel(self,inputSize):
+        network = input_data(shape=[None, inputSize, 1], name = 'input')
 
-	print('Average Accpeted score: ', mean(acceptedScores))
-	print('Median accpeted score: ', median(acceptedScores))
-	print(Counter(acceptedScores))
-	return trainingData
+        network = fully_connected(network, 8, activation = 'relu')		
 
-def BuildNeuralNetworkModel(inputSize):
-	network = input_data(shape=[None, inputSize, 1], name = 'input')
+        network = fully_connected(network, 2, activation='softmax')
+        network = regression(network, optimizer='adam', learning_rate=self.learnRate,
+                            loss='categorical_crossentropy', name='targets')
+        model = tflearn.DNN(network, tensorboard_dir='log')
 
-	network = fully_connected(network, 128, activation = 'relu')		
-	network = dropout(network, 0.8)
+        return model
 
-	network = fully_connected(network, 256, activation = 'relu')		
-	network = dropout(network, 0.8)
+    def TrainModel(self,training_data, model=False):
 
-	network = fully_connected(network, 512, activation = 'relu')		
-	network = dropout(network, 0.8)
+        X = np.array([i[0] for i in training_data]).reshape(-1,len(training_data[0][0]),1)
+        y = [i[1] for i in training_data]
 
-	network = fully_connected(network, 254, activation = 'relu')		
-	network = dropout(network, 0.8)
+        if not model:
+            model = self.BuildNeuralNetworkModel(inputSize = len(X[0]))
 
-	network = fully_connected(network, 128, activation = 'relu')		
-	network = dropout(network, 0.8)
+        model.fit({'input': X}, {'targets': y}, n_epoch=4, snapshot_step=500)
+        return model   
 
-	network = fully_connected(network, 2, activation='softmax')
-	network = regression(network, optimizer='adam', learning_rate=learnRate,
-						loss='categorical_crossentropy', name='targets')
-	model = tflearn.DNN(network, tensorboard_dir='log')
+    def Solve(self):     
+        trainingData = self.InitialPopulation()
+        model = self.TrainModel(trainingData)
 
-	return model
+        scores = []
+        choices = []
+        for each_game in range(100):
+            score = 0
+            game_memory = []
+            prev_obs = []
+            self.enviroment.reset()
+            done = False
+            while not done:
+                self.enviroment.render()
 
-def BuildKerasNeuralNetworkModel(inputSize):
-	model = Sequential()
-	model.add(Dense(128, activation='relu', input_shape=[None, inputSize, 1]))
-	model.add(Dropout(0.8))
+                if len(prev_obs)==0:
+                    action =  self.enviroment.action_space.sample()
+                else:
+                    action = np.argmax(model.predict(prev_obs.reshape(-1,len(prev_obs),1))[0])
 
-	model.add(Dense(256, activation='relu'))
-	model.add(Dropout(0.8))
+                choices.append(action)
+                        
+                new_observation, reward, done, info = self.enviroment.step(action)
+                prev_obs = new_observation
+                game_memory.append([new_observation, action])
+                score+=reward
+                if done: break
+            
+            print('trial: {} score: {}'.format(each_game,score))
+            scores.append(score)
 
-	model.add(Dense(512, activation='relu'))
-	model.add(Dropout(0.8))
+        print('Average Score:',sum(scores)/len(scores))
+        print('choice 1:{}  choice 0:{}'.format(choices.count(1)/len(choices),choices.count(0)/len(choices)))
+        print(self.scoreRequirement)
 
-	model.add(Dense(256, activation='relu'))
-	model.add(Dropout(0.8))
-
-	model.add(Dense(128, activation='relu'))
-	model.add(Dropout(0.8))
-
-	model.add(Dense(2, activation='softmax'))
-	model.compile(optimizer='adam', learning_rate=learnRate, loss='categorical_crossentropy', name='targets')
-	return model
-
-
-BuildNeuralNetworkModel(50)
+cartpole = Cartpole()
+cartpole.Solve()
