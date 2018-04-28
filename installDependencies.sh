4bc137b63deb7df124e55e374adab313f249c948
Wesley Burlani <wesleyburlani@Wesleys-MacBook-Air.local>
2018-04-28 14:16:44 -0300

    .

--- installDependencies.sh
+++ installDependencies.sh
@@ -1,1 +1,2 @@
-pip install -U -r dependencies/requirements.txt --disable-pip-version-check 
+echo 'checking for dependencies...'
+pip install -U -r dependencies/requirements.txt --disable-pip-version-check --exists-action w | grep -v 'Requirement already up-to-date' 
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 sh ./installDependencies.sh
-python src/cartpole.py
+python -W ignore src/cartpole.py | grep -v 'not supported' | grep -v 'WARN' | grep -v 'tensorflow' 
